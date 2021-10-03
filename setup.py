18be168a7b2bbf68a5964863e819dcca32e602e3
Ahmad Houri <houri@future-demand.com>
2021-10-03 21:09:24 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kafka-producer',
+    version='1.0.0',
+    description='Kafka Producer',
+    url='https://github.com/ahmadhori/kafka-producer',
+    author='Ahmad Houri',
+    classifiers=['Kafka Producer Consumer'],
+    keywords='Kafka',
+    packages=find_packages(),
+    install_requires=[],
+)
